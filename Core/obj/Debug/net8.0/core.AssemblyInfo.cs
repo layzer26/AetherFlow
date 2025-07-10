f517257cf8d155534bd048f3f9b6c41a0a8c2e13
Liam Naidoo <lays13n+github@gmail.com>
2025-07-10 12:08:01 +0200

    Add LICENSE.txt with usage restrictions
    Introduced a LICENSE.txt file specifying copyright, usage restrictions, and contact information for licensing. No functional code changes were made.

--- Core/obj/Debug/net8.0/core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a2cb280bafc55d654362177b7216de170e174e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f86c80e796e8009cd262449ff902a75baaec10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
